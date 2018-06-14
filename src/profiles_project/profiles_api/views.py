ce86e3ddeaa2a75745bdd12911c92cf9b3927b38
Zakaria Morchid <morchid.zakariaa@gmail.com>
2018-06-14 16:41:33 +0200

    Added login API

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers
 from . import models
@@ -110,11 +112,22 @@ class HelloViewSet(viewsets.ViewSet):
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-        """Handel creating, updating deleting profiles."""
+    """Handel creating, updating deleting profiles."""
 
-        serializer_class = serializers.USerProfileSerializer
-        queryset = models.UserProfile.objects.all()
-        authentication_classes = (TokenAuthentication,)
-        permission_classes = (permissions.UpdateOwnProfile,)
-        filter_backends = (filters.SearchFilter,)
-        search_fields = ('email', 'name',)
+    serializer_class = serializers.USerProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('email', 'name',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email and password and returns an auth token"""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainAuthToken APIView to validate and create a token."""
+
+        return ObtainAuthToken().post(request)
